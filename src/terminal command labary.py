6adb90e6bc417c75bb80b37eb72b69c2bd704bfc
admin <rupalisatpute1998@gmail.com>
2022-10-11 17:16:51 +0530

    ok

---
+++ src/terminal command labary.py
@@ -0,0 +1,15 @@
+for xl file:
+pip install xlrd
+pip uninstall xlrd
+
+pytest commamd:
+
+test_arithmetic_functions.py::test_multiplication
+pytest -vv -s -k "arithmetic or pytest" --disable-pytest-warnings .\src\
+pytest -vv -s -k "arithmetic and pytest" --disable-pytest-warnings .\src\
+pytest -vv -s -m Smoke --disable-pytest-warnings .\src\
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+pytest -vv -s --maxfail=2 --disable-pytest-warnings .\src\test_arithmetic_functions.
+pytest -vv -s --disable-pytest-warnings .\src\test_arithmetic_functions.p
--- src/test_pytest_parameterization.py
+++ src/test_pytest_parameterization.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -7,12 +8,11 @@ from selenium.webdriver.common.by import By
 
 test_data = [["user1", "pass1"],
              ["user2", "pass2"],
-             ["user3", "pass3"],
-             ["user4", "pass4"],
-             ["user5", "pass5"],
-             ["user6", "pass6"],
+              ["user3", "pass3"],
+              ["user4", "pass4"],
+              ["user5", "pass5"],
+              ["user6", "pass6"],
              ]
-
 
 @pytest.mark.parametrize("username, password", test_data)
 def test_login(username, password):
@@ -23,5 +23,7 @@ def test_login(username, password):
     driver.find_element(By.ID, "password").clear()
     driver.find_element(By.ID, "password").send_keys(password)
     time.sleep(2)
-    driver.quit()
 
+    # If username and password are correct
+    # user lands on homepage
+    driver.quit()
